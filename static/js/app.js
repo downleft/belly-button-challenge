451459c14ab6bbd4d73acc1171180faf6312d735
downleft <downleft@jodys-air.attlocal.net>
2024-03-25 18:46:58 -0400

    Dropdown Filled

--- static/js/app.js
+++ static/js/app.js
@@ -6,34 +6,28 @@ const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
 
 // Fetch the JSON data
 d3.json(url).then(function(data) {
-    belly_button_data = data;
     names = data.names;
     metadata = data.metadata;
     samples = data.samples;
     console.log(samples)
+
+    // Populate dropdown menu
+    for (let j = 0; j < names.length; j++) {
+        d3.select("select").append("option").text(names[j]);
+      }
+
+    // Create function to set up graph
+    
 });
 
-// async function main() {
-//     await fetchData;
-//     var names = belly_button_data.names;
-//     console.log(names)
+function optionChanged() {
+    let dropdownMenu = d3.select("#selDataset");
+    let dataset = dropdownMenu.property("value");
+    for (let i = 0, i < names.length, i++){
+        if (dataset == names[i]) {
+            d3.select("#sample-metadata").append(metadata[i])
+    }
+    }
+};
 
-// }
-// main() 
-
-// Populate dropdown menu
-
-  
-  //var dropdownContent = document.querySelector('.dropdown-content');
-  
-  //for (i = 0; i < areaNames.length; i++) {
-  
-//     var element = areaNames[i];
-  
-//     var htmlToAppend = document.createElement('a');
-//     htmlToAppend.innerHTML = element.B;
-//     htmlToAppend.href = element.C;
-    
-//     dropdownContent.appendChild(htmlToAppend);
-//   }
 //console.log(belly_button_data)
